2682ad8c57bdf4df5bcb920e0226171467875123
Peter Foley <pwfoley@gmail.com>
2019-11-11 16:19:43 -0800

    download gpx instead of fit, work outside rstudio

--- 1_scripts/get_data.R
+++ 1_scripts/get_data.R
@@ -2,15 +2,19 @@
 library(here)
 library(reticulate)
 library(keyring)
+library(purrr)
 library(dplyr)
 
 # get garmin creds
 gc_user <- "pwfoley@gmail.com"
 key_name <- paste0("gcexport ", gc_user)
-gc_pass <- rstudioapi::askForSecret(
-  key_name,
-  message = paste("Garmin Connect password for ", gc_user, ":", sep = ""),
-  title = paste("Garmin Connect Password"))
+safeSecretGetter <- possibly(
+  rstudioapi::askForSecret,
+  otherwise=keyring::key_get("RStudio Keyring Secrets", key_name))
+gc_pass <- safeSecretGetter(
+    key_name,
+    message = paste("Garmin Connect password for ", gc_user, ":", sep = ""),
+    title = paste("Garmin Connect Password"))
 if(is.null(gc_pass)) {
   stop(paste0("No password supplied for Garmin Connect user ", gc_user))
 }
@@ -25,10 +29,9 @@ export_args <- c(
   "--username", gc_user,
   "--password", gc_pass,
   "--count", max_activities,
-  "--format", "original",
+  "--format", "gpx",
   "--directory", output_dir,
-  "--subdir", "{YYYY}/{MM}",
-  "--start_activity_no", "365",
+  "--subdir", "gpx",
   "--unzip"
 )
 res <- gcexport$main(c("gcexport.py",export_args))
